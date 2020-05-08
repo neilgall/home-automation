025bbaad15c27d0c78562b3b0c591fa01142a8bc
Neil Gall <neil@gall.scot>
2020-05-08 21:49:24 +0100

    Fix network restart script to use netctl

--- roles/flaky-network/files/scripts/network-restart.sh
+++ roles/flaky-network/files/scripts/network-restart.sh
@@ -4,7 +4,5 @@ ping -c2 192.168.0.1 > /dev/null
  
 if [ $? != 0 ]; then
 	echo "Restarting wlan0"
-	nmcli device disconnect wlan0
-	sleep 5
-	nmcli device connect wlan0
+	netctl restart wlan0-FortyEight
 fi
